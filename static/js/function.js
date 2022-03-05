5813930be371a284d37cfd53c86a47b6e0b23ba0
Gilbert Veitia <gveitia13@gmail.com>
2022-03-05 17:20:00 -0500

    ultima vista casi ready

--- static/js/function.js
+++ static/js/function.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+  d = document
   $('.select2').select2({
-    dropdownAutoWidth : false
+    dropdownAutoWidth: false
   });
 
   $('.selectpicker').selectpicker();
 
+  $('.card-mini').on('click', function () {
+    d.querySelectorAll('.card-mini').forEach(e => e.classList.remove('active'))
+    this.classList.add('active')
+    d.querySelector('#prod-img-full').src = this.src
+  })
 });
