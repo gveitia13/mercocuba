64bf198e2623d3d98d0c0326e7773c610e3cc16a
Gilbert Veitia <gveitia13@gmail.com>
2022-03-11 03:42:22 -0500

    templates por componentes

--- mercocuba/urls.py
+++ mercocuba/urls.py
@@ -3,8 +3,8 @@ from mercocuba.views import *
 
 urlpatterns = [
     path('cart_details/', CartDetails.as_view(), name='cart_details'),
-    path('tienda_details/', TiendaDetails.as_view(), name='tienda_details'),
-    path('home/', Home.as_view(), name='home'),
+    path('store_details/', TiendaDetails.as_view(), name='tienda_details'),
+    path('', Home.as_view(), name='home'),
     path('product_details/', ProductDetails.as_view(), name='product_details'),
     path('all_stores/', AllStores.as_view(), name='all_stores'),
 ]
--- mercocuba/views.py
+++ mercocuba/views.py
@@ -1,24 +1,46 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import TemplateView
 
 
 class CartDetails(TemplateView):
-    template_name = ''
+    template_name = 'mercocuba/detalle_carrito.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Detalles del carrito'
+        return context
 
 
 class TiendaDetails(TemplateView):
-    template_name = ''
+    template_name = 'mercocuba/detalle_de_tienda.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Detalle de Tienda'
+        return context
 
 
 class Home(TemplateView):
-    template_name = ''
+    template_name = 'mercocuba/inicio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Inicio'
+        return context
 
 
 class ProductDetails(TemplateView):
-    template_name = ''
+    template_name = 'mercocuba/detalle_de_producto.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Detalles del Producto'
+        return context
 
 
 class AllStores(TemplateView):
-    template_name = ''
+    template_name = 'mercocuba/todas_las_tiendas.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Todas las tiendas'
+        return context
