da0b701dc812011337598b813ec716210cd0df36
Gilbert Veitia <gveitia13@gmail.com>
2022-03-13 15:19:26 -0400

    scroll horizontal navbar v2

--- static/js/function.js
+++ static/js/function.js
@@ -12,4 +12,54 @@ $(document).ready(function () {
     d.querySelector('#prod-img-full').src = this.src
   })
   $('.card-mini:first').click()
+
+  if (isMobile()) {
+    document.querySelectorAll('.swiper-wrapper').forEach(e =>
+      e.style += 'display: flex;' +
+        'flex-wrap: nowrap;' +
+        'overflow-x: auto;' +
+        ' -webkit-overflow-scrolling: touch;')
+    document.querySelectorAll('.swiper-slide').forEach(e =>
+      e.style += 'display: flex;' +
+        'flex-wrap: nowrap;' +
+        'overflow-x: auto;' +
+        'flex: 0 0 auto;')
+  } else {
+
+/*    let cant = 9
+    if (window.outerWidth < 1200)
+      cant = 8
+    if (window.outerWidth < 1000)
+      cant = 7
+    if (window.outerWidth < 800)
+      cant = 6
+    if (window.outerWidth < 700)
+      cant = 5
+    if (window.outerWidth < 600)
+      cant = 4
+    if (window.outerWidth < 500)
+      cant = 3
+
+    console.log(window.outerWidth)
+    console.log(cant)
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: cant,
+        spaceBetween: 15,
+        freeMode: true,
+        pagination: {
+          clickable: true,
+        },
+      })*/
+  }
 });
+
+function isMobile() {
+  return (
+    (navigator.userAgent.match(/Android/i)) ||
+    (navigator.userAgent.match(/webOS/i)) ||
+    (navigator.userAgent.match(/iPhone/i)) ||
+    (navigator.userAgent.match(/iPod/i)) ||
+    (navigator.userAgent.match(/iPad/i)) ||
+    (navigator.userAgent.match(/BlackBerry/i))
+  );
+}
