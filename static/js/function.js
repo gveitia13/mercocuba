0504fb958eea1892c43b6d2e11c3bc4711ea702f
Gilbert Veitia <gveitia13@gmail.com>
2022-03-20 02:20:17 -0400

    ya funciona perfect el zoom

--- static/js/function.js
+++ static/js/function.js
@@ -1,17 +1,45 @@
+const d = document
+
 $(document).ready(function () {
-  const d = document
   $('.select2').select2({
     dropdownAutoWidth: false
   });
 
   $('.selectpicker').selectpicker();
 
-  $('.card-mini').on('click', function () {
+  let evt = new Event()
+  m = new Magnifier(evt);
+  // m.attach({
+  //   thumb: '#prod-img-full',
+  //   large: d.querySelector('#prod-img-full').src,
+  //   mode: 'inside',
+  //   zoom: 2,
+  //   zoomable: true
+  // })
+
+  // try {
+  d.querySelectorAll('.card-mini').forEach(a => a.addEventListener('click', function () {
     d.querySelectorAll('.card-mini').forEach(e => e.classList.remove('active'))
     this.classList.add('active')
     d.querySelector('#prod-img-full').src = this.src
-  })
+    lupa(evt)
+
+    let asd = d.querySelectorAll('#prod-img-full-lens')
+    for (let i = 1; i < asd.length; i++) {
+      asd[i].remove()
+    }
+    let img_large = d.querySelectorAll('#prod-img-full-large')
+    img_large.forEach(e => {
+      if (d.querySelector('#prod-img-full').src !== e.src)
+        e.remove()
+    })
+  }))
+
   $('.card-mini:first').click()
+  // } catch (e) {
+  //   console.log(e)
+  // }
+
 
   /*  if (isMobile()) {
       document.querySelectorAll('.swiper-wrapper').forEach(e =>
@@ -71,6 +99,8 @@ $(document).ready(function () {
         .focus()
     }
   })
+  // magnify("prod-img-full", 2)
+
 });
 
 function isMobile() {
@@ -83,3 +113,84 @@ function isMobile() {
     (navigator.userAgent.match(/BlackBerry/i))
   );
 }
+
+function lupa(evt) {
+  m = new Magnifier(new Event());
+  m.attach({
+    thumb: '#prod-img-full',
+    large: d.querySelector('#prod-img-full').src,
+    mode: 'inside',
+    zoom: 2,
+    zoomable: true
+  })
+}
+
+/*function magnify(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+
+  /!* Create magnifier glass: *!/
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+
+  /!* Insert magnifier glass: *!/
+  img.parentElement.insertBefore(glass, img);
+
+  /!* Set background properties for the magnifier glass: *!/
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+
+  /!* Execute a function when someone moves the magnifier glass over the image: *!/
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+
+  /!*and also for touch screens:*!/
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+
+  function moveMagnifier(e) {
+    var pos, x, y;
+    /!* Prevent any other actions that may occur when moving over the image *!/
+    e.preventDefault();
+    /!* Get the cursor's x and y positions: *!/
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    /!* Prevent the magnifier glass from being positioned outside the image: *!/
+    if (x > img.width - (w / zoom)) {
+      x = img.width - (w / zoom);
+    }
+    if (x < w / zoom) {
+      x = w / zoom;
+    }
+    if (y > img.height - (h / zoom)) {
+      y = img.height - (h / zoom);
+    }
+    if (y < h / zoom) {
+      y = h / zoom;
+    }
+    /!* Set the position of the magnifier glass: *!/
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /!* Display what the magnifier glass "sees": *!/
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    /!* Get the x and y positions of the image: *!/
+    a = img.getBoundingClientRect();
+    /!* Calculate the cursor's x and y coordinates, relative to the image: *!/
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /!* Consider any page scrolling: *!/
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x: x, y: y};
+  }
+}*/
